f7c14e69f8623cd2b1da7e454e7a5c7c6dbeec12
EragonBrisingr <geometrydasher26@gmail.com>
2024-06-04 00:13:32 +0300

    basic login/signup

--- login.php
+++ login.php
@@ -1,2 +1,24 @@
 <?php
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
+    $conn = new mysqli('localhost', 'root', '', 'project');
+    if($conn->connect_error){
+        die('Connection Failed : '.$conn->connect_error);
+    }
+
+    $sqlSearch = "Select * from login
+                where email = '$email' and password = '$password'";
+    
+    $result = mysqli_query($conn, $sqlSearch);
+
+    if(mysqli_num_rows($result) == 1){
+        echo "Login Successful";
+        header("Location: index.html");
+    }
+    else{
+        echo "<script>alert('Login Failed'); window.location.href = 'login.html';</script>";
+    }
+
+    mysqli_close($conn);
+?>
--- signup.php
+++ signup.php
@@ -1,3 +1,36 @@
 <?php
 
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
+    $host = "localhost";
+    $db = "project";
+    $pwd = "";
+    $user = "root";
+
+    $conn = new mysqli($host, $user, $pwd, $db);
+
+    if($conn->connect_error){
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sqlSearch = "Select * from login
+                where username = '$username' or email = '$email'";
+    $result = mysqli_query($conn, $sqlSearch);
+
+    if(mysqli_num_rows($result) > 0){
+        echo "Username or email already exists";
+    }
+    else{
+        $sql = "Insert into login(username, email, password)
+                values('$username', '$email', '$password')";
+        if($conn->query($sql) === TRUE){
+            echo "<script>alert('Signup Successful!'); window.location.href = 'index.html';</script>";
+        }
+        else{
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+    }
+    mysqli_close($conn);
+?>
